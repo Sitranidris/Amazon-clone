71956e1134b241d52fd6a295e89ec6aa149f188a
Sitranidris <151416139+Sitranidris@users.noreply.github.com>
2024-01-20 12:24:22 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import { BrowserRouter as Router, Route , Routes } from 'react-router-dom';
+
+// componenets 👉
+import './components/custom/css/style.css';
+import Header from './components/Header/Header';
+import Check_out from './components/Check_out/Check_out';
+import Banner from './components/Banner/Banner';
+import Login from './components/Login/Login';
+import Payment from './components/Payment/Payment';
+import Orders from './components/Orders & Order/Orders';
+import { auth } from './components/Firebase/firebase';
+import { useStateValue } from './components/State_provider/State_provider';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [{ }, dispatch] = useStateValue();
+
+  const promise = loadStripe(
+    'pk_test_51NOv5GDeBw6wvfnK4fQ9iA4qMReqny5x131whKYRTofRZWCGr9My2I2EFV3nk6iDFnaygrwHxrzeB8Q4TVtHj4Bh00Fd1OpctI'
   );
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser,
+        });
+      } else {
+          dispatch({
+            type: 'SET_USER',
+            user: null,
+          });
+      }
+    });
+  }, []);
+  
+  return (
+    <Router>
+      <div className="App">       
+        <Routes>
+          <Route path="/payment" element={
+          <><Header /> 
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements> </>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/orders" element={<><Header /> <Orders /></>} />
+          <Route path="/check_out" element={<><Header /> <Check_out /></>} />
+          <Route path="/" element={<><Header /> <Banner /></>} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
